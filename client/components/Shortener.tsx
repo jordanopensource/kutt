b63331cf94e16db148b28aad5d4cf3bce6a95a1f
alsahawneh <marina.alsahawneh@gmail.com>
2022-06-23 01:20:58 +0300

    Shortener - Increase margin top

--- client/components/Shortener.tsx
+++ client/components/Shortener.tsx
@@ -185,7 +185,7 @@ const Shortener = () => {
   );
 
   return (
-    <Col width={800} maxWidth="100%" px={[3]} flex="0 0 auto" mt={4}>
+    <Col width={800} maxWidth="100%" px={[3]} flex="0 0 auto" mt={6}>
       <Flex
         as="form"
         id="shortenerform"
